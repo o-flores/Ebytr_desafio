15e817df4710b1f811baa74564662dde4e55212c
Orlando Flores <orlando_flores@id.uff.br>
2021-10-30 01:34:05 -0300

    feat: cria controller para as tasks

---
+++ back-end/controllers/task.js
@@ -0,0 +1,74 @@
+const service = require('../services/task');
+
+const create = async (req, res) => {
+  const {
+    name,
+    description,
+    createdAt,
+    status,
+    updatedAt,
+  } = req.body;
+
+  const data = {
+    name, description, createdAt, status, updatedAt,
+  };
+  await service.create(data);
+  res.status(200).json(data);
+};
+
+const getAll = async (_req, res) => {
+  const response = await service.getAll();
+  res.status(200).json(response);
+};
+
+const getById = async (req, res, next) => {
+  const { id } = req.body;
+
+  const response = await service.getById(id);
+
+  if (response.code) return next(response);
+
+  return res.status(200).json(response);
+};
+
+const update = async (req, res, next) => {
+  const {
+    name,
+    description,
+    createdAt,
+    status,
+    updatedAt,
+  } = req.body;
+
+  const { id } = req.params;
+
+  const data = {
+    name,
+    description,
+    createdAt,
+    status,
+    updatedAt,
+    id,
+  };
+  const response = await service.update(data);
+
+  if (response.code) return next(response);
+  return res.status(200).json(response);
+};
+
+const deleteById = async (req, res, next) => {
+  const { id } = req.params;
+  const response = await service.deleteById(id);
+
+  if (response.code) return next(response);
+
+  return res.status(200).json(response);
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+  update,
+  deleteById,
+};
