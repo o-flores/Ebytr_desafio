cb226e7a13c01dc9e9512ebd1ed0d1af61af685c
Orlando Flores <orlando_flores@id.uff.br>
2021-10-30 01:33:37 -0300

    fix: o id q sera controlado nao sera o gerado pelo mongo

--- back-end/models/tasks.js
+++ back-end/models/tasks.js
@@ -1,28 +1,24 @@
-const { ObjectId } = require('mongodb');
 const { getConnection } = require('./connection');
 
 const DB_COLLECTION = 'tasks';
 
 const create = async (data) => {
   const db = await getConnection();
-  const { insertedId: id } = await db.collection(DB_COLLECTION).insertOne(data);
+  await db.collection(DB_COLLECTION).insertOne(data);
   return {
-    id,
     ...data,
   };
 };
 
 const getAll = async () => {
   const db = await getConnection();
-  const tasks = await db.collection(DB_COLLECTION).find({});
+  const tasks = await db.collection(DB_COLLECTION).find({}).toArray();
   return tasks;
 };
 
 const getById = async (id) => {
-  if (!ObjectId.isValid(id)) return false;
-
   const db = await getConnection();
-  const task = await db.collection(DB_COLLECTION).findOne({ _id: ObjectId(id) });
+  const task = await db.collection(DB_COLLECTION).findOne({ taskId: id });
 
   if (!task) return false;
 
@@ -31,11 +27,10 @@ const getById = async (id) => {
 
 const update = async (data) => {
   const { id, ...newValues } = data;
-  if (!ObjectId.isValid(id)) return false;
 
   const db = await getConnection();
   const { value } = await db.collection(DB_COLLECTION).findOndeAndUpdate(
-    { _id: ObjectId(id) },
+    { taskId: id },
     { $set: { ...newValues } },
     { returnDocument: 'after' },
   );
@@ -43,10 +38,8 @@ const update = async (data) => {
 };
 
 const deleteById = async (id) => {
-  if (!ObjectId.isValid(id)) return false;
-
   const db = await getConnection();
-  const { value } = await db.collection('products').findOneAndDelete({ _id: ObjectId(id) });
+  const { value } = await db.collection('products').findOneAndDelete({ taskId: id });
 
   return value;
 };
