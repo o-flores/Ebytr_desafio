fcf2197a7b5c0794d9b1835427f09f67643e3955
Orlando Flores <orlando_flores@id.uff.br>
2021-10-31 22:04:44 -0300

    feat: cria validação para o update da tarefa

--- back-end/middlewares/taskValidation.js
+++ back-end/middlewares/taskValidation.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 const validator = require('express-joi-validation').createValidator({ passError: true });
 
-const bodySchema = Joi.object({
+const createTaskSchema = Joi.object({
   taskId: Joi.string().required(),
   description: Joi.string().required(),
   createdAt: Joi.date().required(),
@@ -10,4 +10,19 @@ const bodySchema = Joi.object({
   dueDate: Joi.date().required(),
 });
 
-module.exports = validator.body(bodySchema);
+const updateTaskSchema = Joi.object({
+  description: Joi.string().required(),
+  createdAt: Joi.date().required(),
+  status: Joi.string().required(),
+  updatedAt: Joi.date().required(),
+  dueDate: Joi.date().required(),
+});
+
+const createTaskValidation = validator.body(createTaskSchema);
+
+const updateTaskValidation = validator.body(updateTaskSchema);
+
+module.exports = {
+  createTaskValidation,
+  updateTaskValidation,
+};
