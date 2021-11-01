0f4a486e3ee00063b35fecd8ecd1e36f79212af7
Orlando Flores <orlando_flores@id.uff.br>
2021-10-31 21:19:02 -0300

    feat: adiciona o tratamento de erro Joi no middleware de erro

--- back-end/middlewares/error.js
+++ back-end/middlewares/error.js
@@ -1,5 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
+  if (err.error.isJoi) {
+    return res.status(400).json({ error: { message: err.error.details[0].message } });
+  }
   const { code, message } = err;
   return res.status(code).json({ error: { message } });
 };
