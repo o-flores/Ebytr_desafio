cff9b261f0ef592cac9313971644e41fe8f76ec4
Orlando Flores <orlando_flores@id.uff.br>
2021-10-31 22:05:27 -0300

    fix: modifica validações para saber se o erro foi do joi

--- back-end/middlewares/error.js
+++ back-end/middlewares/error.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
-  if (err.error.isJoi) {
+  if (err.error || err.error.isJoi) {
     return res.status(400).json({ error: { message: err.error.details[0].message } });
   }
   const { code, message } = err;
