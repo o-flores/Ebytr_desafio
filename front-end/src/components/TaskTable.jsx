8dbf644b6c37661cc984fa7b7dd93a88f6cfd044
Orlando Flores <orlando_flores@id.uff.br>
2021-11-03 18:19:56 -0300

    feat: adiciona edição para colunas e cria função para mandar os dados atualizados para a api

--- front-end/src/components/TaskTable.jsx
+++ front-end/src/components/TaskTable.jsx
@@ -1,19 +1,21 @@
-import * as React from 'react';
+import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
-const columns = [
+const tableColumns = [
   {
     field: 'id', headerName: 'ID', width: 95,
   },
-  { field: 'description', headerName: 'Description', width: 150 },
+  {
+    field: 'description', headerName: 'Description', width: 150, editable: true,
+  },
   {
     field: 'createdAt', headerName: 'created At', width: 150, type: 'date',
   },
   {
-    field: 'status', headerName: 'Status', width: 150, type: 'singleSelect', valueOptions: ['Em andamento', 'Finalizada', 'Não iniciada'],
+    field: 'status', headerName: 'Status', width: 150, type: 'singleSelect', valueOptions: ['Em andamento', 'Finalizada', 'Não iniciada'], editable: true,
   },
   {
-    field: 'dueDate', headerName: 'Due date', width: 150, type: 'date',
+    field: 'dueDate', headerName: 'Due date', width: 150, type: 'date', editable: true,
   },
   {
     field: 'updatedAt', headerName: 'Last modification', width: 150, type: 'date',
@@ -22,20 +24,41 @@ const columns = [
 
 const rows = [
   {
-    id: 1, description: 'lavar louça', createdAt: '30-11-2020', status: 'Easdq', dueDate: '31-12-2020', updatedAt: '30-11-2020',
+    id: 1, description: 'lavar louça', createdAt: '2020-11-30', status: 'Finalizada', dueDate: '2020-12-30', updatedAt: '2020-11-30',
+  },
+  {
+    id: 2, description: 'lavar louça', createdAt: '2020-11-30', status: 'Finalizada', dueDate: '2020-12-30', updatedAt: '2020-11-30',
   },
 ];
 
 function TaskTable() {
+  const handleEditStop = (params) => {
+    const { getValue, id, columns } = params;
+    const columnsName = columns.map((key) => key.field);
+    const values = {};
+
+    for (let i = 0; i < columnsName.length; i += 1) {
+      const key = columnsName[i];
+      if (columnsName[i] !== 'id') {
+        values[key] = getValue(id, key);
+      }
+    }
+    // console.log(values);
+    // fazer a chamada para a api para atualizar uma tarefa
+  };
+
   return (
     <div style={{ width: '80%', margin: 'auto' }}>
       <DataGrid
         rows={rows}
-        columns={columns}
+        columns={tableColumns}
         pageSize={10}
         rowsPerPageOptions={[10]}
-        checkboxSelection
         autoHeight
+        disableSelectionOnClick
+        disableMultipleSelection
+        editMode="row"
+        onRowEditStop={handleEditStop}
       />
     </div>
   );
