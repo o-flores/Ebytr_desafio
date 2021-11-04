50245a13f246c1c7fd5eca4be8d90ad1eae833b9
Orlando Flores <orlando_flores@id.uff.br>
2021-11-04 19:49:49 -0300

    feat: renderiza Header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import TaskTable from './components/TaskTable';
 
 function App() {
   return (
-    <TaskTable />
+    <>
+      <Header />
+      <TaskTable />
+    </>
   );
 }
 
