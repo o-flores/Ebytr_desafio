d98ba9b513611ec24d370f98950696471fd7ee23
Orlando Flores <orlando_flores@id.uff.br>
2021-11-04 19:49:38 -0300

    feat: cria header para o app

---
+++ front-end/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { Typography } from '@material-ui/core';
+
+function Header() {
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        marginBottom: 5, backgroundColor: '#808080', display: 'flex', alignItems: 'center', padding: 2,
+      }}
+    >
+      <Toolbar>
+        <Typography variant="h3" component="div">
+          TODO LIST
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
