e391e6bcf65cb9cbd1565e1438d451474b0d159e
Orlando Flores <orlando_flores@id.uff.br>
2021-11-03 22:50:24 -0300

    fix: corrige bug  q dava quando o id nao existia no banco

--- back-end/middlewares/error.js
+++ back-end/middlewares/error.js
@@ -1,8 +1,11 @@
+/* eslint-disable consistent-return */
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
-  if (err.error || err.error.isJoi) {
+  if (err.code) {
+    const { code, message } = err;
+    return res.status(code).json({ error: { message } });
+  }
+  if (err.error.isJoi || err.error) {
     return res.status(400).json({ error: { message: err.error.details[0].message } });
   }
-  const { code, message } = err;
-  return res.status(code).json({ error: { message } });
 };
